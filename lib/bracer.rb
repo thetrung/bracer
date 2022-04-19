1d0237b579376765a0b7ff22472c37c76fe5ab6b
Jack Dragoon <deulamco@iMac-deulamco.local>
2022-04-19 23:23:57 +0700

    1st: restore repo from gem

---
+++ lib/bracer.rb
@@ -0,0 +1,2 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'bracer', 'bracer_parser.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'bracer', 'bracer.rb'))
---
+++ test/unit/integer_literal_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
+
+class IntegerLiteralTest < Test::Unit::TestCase
+
+  def setup
+    @parser = Bracer.new
+  end
+
+  test "should parse sexp containing an implicitly positive integer literal" do
+    ast = @parser.parse_string("[10]")
+    assert_equal [[10]], ast
+  end
+  
+  test "should parse sexp containing an explicitly positive integer literal" do
+    ast = @parser.parse_string("[+910]")
+    assert_equal [[910]], ast
+  end
+  
+  test "should parse sexp containing an explicitly negative integer literal" do
+    ast = @parser.parse_string("[-10]")
+    assert_equal [[-10]], ast
+  end
+    
+end
